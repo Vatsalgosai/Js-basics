36b523c2cae3eb6de2b35760a8955174753590b5
Vatsalgosai <125549631+Vatsalgosai@users.noreply.github.com>
2023-02-17 12:44:14 +0530

    Add files via upload

---
+++ Prac-8.js
@@ -0,0 +1,13 @@
+let a = [1, 2, 5, 2, 1, 8, 7];
+
+let b = [];
+
+let len = a.length;
+
+for(let i = 0; i<len; i++){
+    if(b.indexOf(a[i]) === -1)
+    {
+        b.push(a[i]);
+    }
+}
+console.log(b);
