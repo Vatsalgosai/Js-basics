a2988a041975e068f11434deaf0954ed89f36080
Vatsalgosai <125549631+Vatsalgosai@users.noreply.github.com>
2023-03-07 16:33:23 +0530

    Update Prac-5.js

--- Prac-5.js
+++ Prac-5.js
@@ -3,7 +3,16 @@ let arr = [2, 5, 8, 3, 1, 4, 6];
 //1+2+3+4+5+6 = 21
 //6*7/2 = 21
 //((n+1)*(n+2))/2=21    n is number of element in array  [here n = 5]
-let arr1 = arr.sort();
+let arr1 = arr.sort((x, y) => {
+    if (x < y) {
+       return -1;
+    }
+    if (x > y) {
+       return 1;
+    }
+    return 0;
+    // return x-y;          // second method
+ });
 
 let n = arr1.length;
 
