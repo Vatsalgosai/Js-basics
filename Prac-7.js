c5e50decf469a0fee3fc927915b1618f94dbc1be
Vatsalgosai <125549631+Vatsalgosai@users.noreply.github.com>
2023-03-07 18:00:25 +0530

    Update Prac-7.js

--- Prac-7.js
+++ Prac-7.js
@@ -1,19 +1,16 @@
 let arr = [ 1, 23, 12, 9, 30, 2, 50 ];
 let k = arr.length;
-let n = 2;
+let n = 3;
 nthLargest(arr, k, n);
 
 function nthLargest(arr, k, n)
 {
     // Sort the given array arr in reverse
     // order.
-    arr.sort((a, b) => b - a);
     if(n>k){
         console.log("number is larger then array size")
     }else{
+        arr.sort((a, b) => b - a);
         console.log(arr[n-1])
     }
 } 
- 
-// driver program
-   
