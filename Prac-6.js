84d1a2f465e18f17a476bed68443633f95c1a5c4
Vatsalgosai <125549631+Vatsalgosai@users.noreply.github.com>
2023-03-09 12:12:06 +0530

    Update Prac-6.js

--- Prac-6.js
+++ Prac-6.js
@@ -1,16 +1,16 @@
-let arr = [1, 2, 3, 4, 5, 6, 8];
+let arr = [1, 2, 3, 4, 5, 6, 8, 15];
 
-//1+2+3+4+5+6 = 21
-//6*7/2 = 21
-//((n+1)*(n+2))/2=21    n is number of element in array  [here n = 5]
+function missing(arr){
+    let missingnum = [];
 
-let n = arr.length;
+    let minNum = Math.min(...arr);
+    let maxNum = Math.max(...arr);
 
-let total = ((n+1)*(n+2))/2;
-
-for(let i = 0; i<n; i++){
-
-    total = total - arr[i];
+    for(let i = minNum; i<maxNum; i++){
+        if(arr.indexOf(i) < 0){
+            missingnum.push(i);
+        }
+    }
+    return missingnum;
 }
-
-console.log(total);
+console.log(missing(arr));
