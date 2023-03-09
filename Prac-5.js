eeecd71cbbb64cbdfd920885d76e40c1aeff02db
Vatsalgosai <125549631+Vatsalgosai@users.noreply.github.com>
2023-03-09 12:11:00 +0530

    Update Prac-5.js

--- Prac-5.js
+++ Prac-5.js
@@ -1,26 +1,13 @@
 let arr = [2, 5, 8, 3, 1, 4, 6];
 
-//1+2+3+4+5+6 = 21
-//6*7/2 = 21
-//((n+1)*(n+2))/2=21    n is number of element in array  [here n = 5]
-let arr1 = arr.sort((x, y) => {
-    if (x < y) {
-       return -1;
-    }
-    if (x > y) {
-       return 1;
-    }
-    return 0;
-    // return x-y;          // second method
- });
+function missing(arr){
+   let sum = 1;
+   let c = 2;
 
-let n = arr1.length;
-
-let total = ((n+1)*(n+2))/2;
-
-for(let i = 0; i<n; i++){
-
-    total = total - arr1[i];
+   for(let i = 0; i < arr.length; i++){
+      sum = sum - arr[i] + c;
+      c++;
+   }
+   console.log(sum);
 }
-
-console.log(total);
+missing(arr);
